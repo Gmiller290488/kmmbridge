9b14da7e3eb8c88b0227e5c321c988d39265ba2b
gareth miller <gareth.miller3@tesco.com>
2022-11-24 09:14:27 +0000

    removed emoji

--- strings/src/commonMain/kotlin/Strings.kt
+++ strings/src/commonMain/kotlin/Strings.kt
@@ -1,8 +1,10 @@
+
 class Strings {
-    val wordleTitle = "Wordle 🎲"
+    val wordleTitle = "Wordle"
     val onboardingDescription = "This is Wordle"
     val onboardingButtonTitle = "Got it"
     val winner = "You won!"
     val loser = "You lost!"
     val notInWordList = "Not in word list"
+    }
 }
